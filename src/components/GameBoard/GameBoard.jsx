bc41abbbee80d031af96e12bd04f68eec163aa4a
Bamberek <kabaczek99@gmail.com>
2024-06-05 01:24:31 +0200

    created game board component

---
+++ src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,24 @@
+const GameBoard = ({ onSquareClick, board }) => {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSquareClick(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
