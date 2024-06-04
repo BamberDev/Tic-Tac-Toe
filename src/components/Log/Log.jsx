3d9049649a804968894ba0489963b8058ca98304
Bamberek <kabaczek99@gmail.com>
2024-06-05 01:24:57 +0200

    created log component

---
+++ src/components/Log/Log.jsx
@@ -0,0 +1,14 @@
+const Log = ({ turns }) => {
+  return (
+    <ol id="log">
+      <h2>Game History</h2>
+      {turns.map((turn) => (
+        <li key={`${turn.square.row},${turn.square.col}`}>
+          {turn.player} placed {turn.square.row},{turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default Log;
