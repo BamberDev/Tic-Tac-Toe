2835598797d49897e99be1934c6bf48e689a2565
Bamberek <kabaczek99@gmail.com>
2024-08-27 23:13:01 +0200

    moved and updated components

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,7 @@
 const Log = ({ turns }) => {
   return (
     <ol id="log">
-      <h2>Game History</h2>
+      <h2>Game History:</h2>
       {turns.map((turn) => (
         <li key={`${turn.square.row},${turn.square.col}`}>
           {turn.player} placed {turn.square.row},{turn.square.col}
